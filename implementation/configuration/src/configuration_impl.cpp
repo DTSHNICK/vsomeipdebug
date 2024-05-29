2faabbb87a1d1f512b607eb47c6388688ebdefd7
Danylo Tsyban <danylo.tsyban@zenseact.com>
2024-05-29 10:02:49 +0200

    First log

--- implementation/configuration/src/configuration_impl.cpp
+++ implementation/configuration/src/configuration_impl.cpp
@@ -829,6 +829,7 @@ configuration_impl::load_routing_host(const boost::property_tree::ptree &_tree,
                 }
             } else if (its_key == "unicast") {
 #if VSOMEIP_BOOST_VERSION < 106600
+                VSOMEIP_INFO << "!unicast: " << its_value;
                 routing_.host_.unicast_
                     = boost::asio::ip::address::from_string(its_value);
 #else
@@ -3107,11 +3108,15 @@ configuration_impl::is_local_routing() const {
 
     bool is_local(true);
     try {
-        is_local = routing_.host_.unicast_.is_unspecified() ||
-                routing_.host_.unicast_.is_multicast();
+        bool is_unspecified = routing_.host_.unicast_.is_unspecified();
+        bool is_multicast = routing_.host_.unicast_.is_multicast();
+        VSOMEIP_INFO << "is_unspecified " << is_unspecified;
+        VSOMEIP_INFO << "unicast: " << routing_.host_.unicast_.to_string();
+        is_local = is_unspecified || is_multicast;
     } catch (...) {
+        VSOMEIP_INFO << "ERRORRR ";
     }
-
+    VSOMEIP_INFO << "is_local " << is_local;
     return is_local;
 }
 
